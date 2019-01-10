72babe0e293ab93c4a43a59794841f8d961514d6
wll <wllnyubupt@gmail.com>
2019-01-09 22:26:53 -0500

    add load config

--- utils/file_system_utils.py
+++ utils/file_system_utils.py
@@ -5,9 +5,20 @@ This script contains all the util functions for file systems.
 import os
 import glob
 
+import yaml
+
 def create_folder(folder_path):
     try:
         os.mkdir(folder_path)
         print("Directory " , folder_path,  " Created ")
     except FileExistsError:
         print("Directory " , folder_path,  " already exists")
+
+
+def load_config(filename):
+    params = {}
+    
+    with open(filename) as f:
+        params = yaml.load(f)
+
+    return params
